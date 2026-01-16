c0cb50582a08532ddfdec66889e52e7e08ccb63a
adamjogkins-eng <adamjogkins@gmail.com>
2026-01-16 23:28:15 +0000

    Refactor iOSStrapMenu for half-screen display
    Updated iOSStrapMenu for a half-screen layout with improved UI elements and responsiveness.

--- iOSStrap.mm
+++ iOSStrap.mm
@@ -12,39 +12,58 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor colorWithWhite:0 alpha:0.85];
+        // Half-screen look with rounded corners
+        self.backgroundColor = [UIColor colorWithWhite:0 alpha:0.9];
+        self.layer.cornerRadius = 30;
+        self.layer.borderWidth = 1.5;
+        self.layer.borderColor = [UIColor systemBlueColor].CGColor;
+        self.clipsToBounds = YES;
         
-        UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, frame.size.width, 40)];
+        // Header
+        UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 15, frame.size.width, 30)];
         title.text = @"BLOCKSS EXECUTOR";
-        title.textColor = [UIColor whiteColor];
+        title.textColor = [UIColor systemBlueColor];
         title.textAlignment = NSTextAlignmentCenter;
-        title.font = [UIFont boldSystemFontOfSize:24];
+        title.font = [UIFont boldSystemFontOfSize:18];
         [self addSubview:title];
 
-        _scriptBox = [[UITextView alloc] initWithFrame:CGRectMake(20, 100, frame.size.width - 40, frame.size.height - 250)];
-        _scriptBox.backgroundColor = [UIColor colorWithWhite:0.1 alpha:0.8];
+        // Adjusted Script Box for half-screen
+        _scriptBox = [[UITextView alloc] initWithFrame:CGRectMake(15, 55, frame.size.width - 30, frame.size.height - 130)];
+        _scriptBox.backgroundColor = [UIColor colorWithWhite:0.05 alpha:1.0];
         _scriptBox.textColor = [UIColor cyanColor];
-        _scriptBox.font = [UIFont fontWithName:@"Courier" size:14];
-        _scriptBox.layer.cornerRadius = 10;
-        _scriptBox.text = @"-- Loadstring / Instance Test\ngame.Workspace.Gravity = 0";
+        _scriptBox.font = [UIFont fontWithName:@"Courier" size:13];
+        _scriptBox.layer.cornerRadius = 12;
+        _scriptBox.text = @"-- Half-Screen Executor Ready\ngame.Workspace.Gravity = 0";
         [self addSubview:_scriptBox];
 
+        // Compact Execute Button
         UIButton *execBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-        execBtn.frame = CGRectMake(frame.size.width/2 - 100, frame.size.height - 100, 200, 50);
+        execBtn.frame = CGRectMake(15, frame.size.height - 65, frame.size.width - 30, 50);
         execBtn.backgroundColor = [UIColor systemBlueColor];
-        execBtn.layer.cornerRadius = 25;
+        execBtn.layer.cornerRadius = 15;
         [execBtn setTitle:@"EXECUTE" forState:UIControlStateNormal];
         [execBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [execBtn.titleLabel setFont:[UIFont boldSystemFontOfSize:16]];
         [execBtn addTarget:self action:@selector(runScript) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:execBtn];
+        
+        // Allow the whole menu to be draggable
+        UIPanGestureRecognizer *menuPan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleMenuPan:)];
+        [self addGestureRecognizer:menuPan];
     }
     return self;
+}
+
+- (void)handleMenuPan:(UIPanGestureRecognizer *)sender {
+    CGPoint translation = [sender translationInView:self.superview];
+    self.center = CGPointMake(self.center.x + translation.x, self.center.y + translation.y);
+    [sender setTranslation:CGPointZero inView:self.superview];
 }
 
 - (void)runScript {
     UIImpactFeedbackGenerator *gen = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleHeavy];
     [gen impactOccurred];
-    NSLog(@"Sending to Lua VM...");
+    NSLog(@"Executing script in half-screen mode...");
 }
 @end
 
@@ -61,6 +80,7 @@ static iOSStrapMenu *sharedMenu;
         [self addGestureRecognizer:pan];
         
         UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(vanish)];
+        longPress.minimumPressDuration = 2.0;
         [self addGestureRecognizer:longPress];
     }
     return self;
@@ -75,6 +95,7 @@ static iOSStrapMenu *sharedMenu;
 - (void)vanish {
     [sharedMenu removeFromSuperview];
     [self removeFromSuperview];
+    AudioServicesPlaySystemSound(1521);
 }
 
 - (void)tapped {
@@ -92,10 +113,12 @@ static void init() {
                 break;
             }
         }
-        
         if (!win) win = [UIApplication sharedApplication].windows.firstObject;
 
-        sharedMenu = [[iOSStrapMenu alloc] initWithFrame:win.bounds];
+        // Creating a half-screen menu (approx 350x400)
+        CGFloat menuWidth = 350;
+        CGFloat menuHeight = 400;
+        sharedMenu = [[iOSStrapMenu alloc] initWithFrame:CGRectMake(win.center.x - (menuWidth/2), win.center.y - (menuHeight/2), menuWidth, menuHeight)];
         sharedMenu.hidden = YES;
         [win addSubview:sharedMenu];
 
